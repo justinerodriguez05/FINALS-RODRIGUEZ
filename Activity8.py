7af6e7bfd1900889bcbc4b903aff5f15b280ada6
justinerodriguez05 <astinweide@gmail.com>
2024-12-15 18:04:42 +0800

    Create Activity8.py

---
+++ Activity8.py
@@ -0,0 +1,9 @@
+Password = input("Please Enter your Password: ")
+
+if Password.lower() == "secret":
+    print("Access Granted")
+    print("Welcome to the System")
+    print("Enjoy using the System")
+else:
+    print("Access Denied!")
+    print("Thank your for using the System!")
