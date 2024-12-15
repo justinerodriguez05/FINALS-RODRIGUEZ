f209223c0a94f7f8b8869d2e926908c455db8220
justinerodriguez05 <astinweide@gmail.com>
2024-12-15 17:47:19 +0800

    Create Activity6.py

---
+++ Activity6.py
@@ -0,0 +1,34 @@
+#Assignment Operators
+x = 10
+print(x)
+
+a = x + 10
+print(a)
+
+c = 100
+c += 100
+print(c)
+
+d =  100
+d -= 100
+print(d)
+
+e = 100
+e *= 100
+print(e)
+
+f = 100
+f /= 100
+print(f)
+
+g = 100
+g //= 100
+print(f)
+
+h = 100
+h **= 1
+print(h)
+
+i = 100
+i %= 100
+print(i)
