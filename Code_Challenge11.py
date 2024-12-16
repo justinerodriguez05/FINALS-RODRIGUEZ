583faf6c1e3db00df20a9fbed6eba2b603899561
justinerodriguez05 <astinweide@gmail.com>
2024-12-16 11:01:59 +0800

    Create Code_Challenge11.py

---
+++ Code_Challenge11.py
@@ -0,0 +1,18 @@
+for a in range(0, 4):
+    for b in range(4, a, -1):
+        print(" ", end=" ")
+    for c in range(0, a, + 1):
+        print("b", end=" ")
+    for c in range(1, a, + 1):
+        print("v", end=" ")
+    print()
+
+
+for e in range(0, 3):
+    for f in range(0, e):
+        print(" ", end=" ")
+    for g in range(3, e, -1):
+        print("y", end=" ")
+    for g in range(2, e, -1):
+        print("l", end=" ")
+    print()
