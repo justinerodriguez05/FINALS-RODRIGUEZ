0004b8d05571253031f9f56b120beed35e9cc67e
justinerodriguez05 <astinweide@gmail.com>
2024-12-16 09:47:17 +0800

    Create Activity23.py

---
+++ Activity23.py
@@ -0,0 +1,12 @@
+def factorial(number):
+    """ This Function again is for calculating Factorial Numbers, just provide a value, and it would automatincally compute the factorial"""
+    fact = 1
+    for x in range(number, 0, -1):
+        fact *= 1
+    return fact
+
+
+
+
+help(factorial)
+help(help)
