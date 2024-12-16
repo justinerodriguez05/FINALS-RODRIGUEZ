8ec1567552a32545324fa6ab8e4a11f8035a83f1
justinerodriguez05 <astinweide@gmail.com>
2024-12-16 10:40:41 +0800

    Create Code_Challenge10.py

---
+++ Code_Challenge10.py
@@ -0,0 +1,17 @@
+for x in range(6, 0, -1):
+    for y in range(1, x + 1):
+        print(" ", end=" ")
+    for z in range(6, x, -1):
+        print("*", end=" ")
+    for a in range(6, x, -1):
+        print("*", end=" ")
+    print()
+
+for x in range(1, 5):
+    for y in range(1, x + 2):
+        print(" ", end=" ")
+    for z in range(5, x, -1):
+        print("*", end=" ")
+    for a in range(5, x, -1):
+        print("*", end=" ")
+    print()
